e7906d0d5112d12f225b2ac93c51280b32ce5b24
RZ <ckallinen@gmail.com>
2024-06-06 16:39:29 -0400

    created a past a specific day

---
+++ PowerShell/2nd-date-true-false.ps1
@@ -0,0 +1,53 @@
+# this script is to determine if the date is true or false if past the targeted date
+
+Param (
+        [Parameter(Mandatory=$true)][ValidateSet("First","Second","Third","Fourth","Last","1","2","3","4","5")][string]$find,
+        [Parameter(Mandatory=$true)][ValidateSet("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")][string]$weekday,
+        [Parameter(Mandatory=$true)][datetime]$date
+        #[Parameter(Mandatory=$true)][ValidatePattern('^\d{4}$')][int]$year,
+        #[Parameter(Mandatory=$true)][ValidatePattern('^\d{2}$')][int]$numday
+    )
+switch($find) 
+    { 
+        "First" { $intFind = 1 }
+        "Second" { $intFind = 2 }
+        "Third" { $intFind = 3 }
+        "Fourth" { $intFind = 4 }
+        "Last" { $intFind = 5 }
+        default { $intFind = [int]$Find }
+    }
+# Function to find target date
+function find-datetarget ([int]$year, [int]$month, [string]$weekdate, [int]$intFind) {
+    
+    $allDays = @()
+    0..31 | ForEach-Object -Process {            
+        $evaldate = (Get-Date -Year $year -Month $month -Day 1).AddDays($_)        
+        if ($evaldate.Month -eq $month)         
+        {
+            if ($evaldate.DayOfWeek -eq $weekdate) { 
+                $allDays += $evaldate  
+            }            
+        }            
+    }
+    if ($allDays.Count -lt $intFind) {$intFind = $intFind-1}
+    $allDays[$($intFind-1)]
+    Return
+}
+
+$output = find-datetarget $date.Year $date.Month $weekday $intFind
+
+switch ($intFind) {
+    1 { $di = "1st" }
+    2 { $di = "2nd" }
+    3 { $di = "3rd" }
+    4 { $di = "4th" }
+    5 { $di = "5th" }
+    Default { $di = "error"}
+}
+
+if ($output -lt $date) {
+    Write-Host -ForegroundColor Green "The date ($date) is passed the $di $weekday of the month"
+} else {
+    Write-Host -ForegroundColor Red "The date ($date) is not passed the $di $weekday of the month"
+}
+
